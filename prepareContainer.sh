4cb7e682192ea05fc448928dd127bffa0f48d2dd
khalil <malikhalil36@gmail.com>
2019-10-19 15:34:30 +0500

    commit

---
+++ prepareContainer.sh
@@ -0,0 +1,14 @@
+# pip install --upgrade pip
+# pip install matplotlib_venn 
+
+
+# pip install keras
+# pip install Pillow
+
+echo 'Installing Dependencies'
+
+pip install xgboost
+# conda install theano
+# conda install -c conda-forge tensorflow
+# conda install graphviz
+# conda install pydotplus -y
